fb38da9fd6c9f8557e591ad22350280780967188
Malek Drebi <73871825+malekdrebi@users.noreply.github.com>
2026-07-08 21:08:42 +0200

    Add cache buster for admin.js, debug logging

--- admin/index.php
+++ admin/index.php
@@ -276,6 +276,6 @@ $csrfToken = Auth::csrfToken();
             setTimeout(onRestaurantChange, 100);
         }
     </script>
-    <script src="assets/admin.js"></script>
+    <script src="assets/admin.js?v=3"></script>
 </body>
 </html>
